5c55e6fd436eeff8a70ed1a7ce4c3d22c67cce28
AllyGarcia <allygarcia152@gmail.com>
2022-04-24 15:46:37 -0500

    add express package and set up server on server.js

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,11 @@
+// require express
+// require lib
+// require db
+
+// GET /notes to return notes.html file
+
+// GET * to return index.html file
+
+// GET /api/notes read db.json file to retreave saved notes
+
+// POST /api/notes receives new notes to save to the request body and add to the db.json file (find npm package that gives each note a unique id)
